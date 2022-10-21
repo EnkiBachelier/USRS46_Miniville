3f1077808fe517fdbc290f79d33400e8e1f5b7c4
Sulivan-Michalon <sulivan.michalon@gmail.com>
2022-10-21 14:37:45 +0200

    Structure de Base
    il y a surement des modifications a faire mais j'ai intégrer ici tout ce qui était monter pas l'UML

--- Miniville_GroupeC/Player.cs
+++ Miniville_GroupeC/Player.cs
@@ -6,5 +6,20 @@ namespace Miniville_GroupeC
 {
     public class Player
     {
+        List<Card> cards = new List<Card> {};
+        int piece;
+        game = Game();
+        string name;
+        pile = Pile();
+
+        public void BuyCard(Card, cards)
+        {
+            
+        }
+
+        public void CheckCardToActivate(int diceValue)
+        {
+
+        }
     }
 }
