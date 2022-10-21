887fb95e968ec4bc88a2c038d99fdc90f1ac6b0e
Nono2606 <115997923+Nono2606@users.noreply.github.com>
2022-10-21 15:25:45 +0200

    Add Dice.cs et verif main

--- Miniville_GroupeC/Dice.cs
+++ Miniville_GroupeC/Dice.cs
@@ -6,5 +6,10 @@ namespace Miniville_GroupeC
 {
     public class Dice
     {
+        Random rdm = new Random();
+
+        private int Dices => rdm.Next(1, 7);
+
+        public int De { get { return Dices; } }
     }
 }
--- Miniville_GroupeC/Program.cs
+++ Miniville_GroupeC/Program.cs
@@ -4,9 +4,12 @@ namespace Miniville_GroupeC
 {
     class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    Console.WriteLine("Hello World!");
-        //}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Dice d = new Dice();
+            Console.WriteLine(d.De);
+        }
     }
 }
