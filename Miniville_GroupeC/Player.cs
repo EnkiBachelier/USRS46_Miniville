864ffc027d45ee3a2a3bf5de4af6a59f93d0713d
Sulivan-Michalon <sulivan.michalon@gmail.com>
2022-10-24 09:32:20 +0200

    test ajout
    a tester je ne sais pas si ca marche j'ai fait quelque test reste a savoir ci cela est bon;

--- Miniville_GroupeC/Player.cs
+++ Miniville_GroupeC/Player.cs
@@ -6,20 +6,36 @@ namespace Miniville_GroupeC
 {
     public class Player
     {
-        List<Card> cards = new List<Card> {};
-        int piece;
-        game = Game();
-        string name;
-        pile = Pile();
-
+		
+		List<Card> Cards = new List<Card> ();
+        int Piece;
+        Game = Game();
+        string Name;
+        Pile = Pile();
+		
+		public Player(int piece,cards[],Game() game,string name, Pile() pile)
+		{
+			this.Piece = piece;
+			this.Cards = cards;
+			this.Game = game;
+			this.Name = name;
+			this.Pile = pile
+		}
+		
         public void BuyCard(Card, cards)
         {
-            
+            //ici rediriger vers la carte en question
+		
+			cards += Card
         }
 
         public void CheckCardToActivate(int diceValue)
         {
-
+			foreach(Card C in cards)
+			{
+				if (diceValue in Costvalue) {return true}
+				else () {return false}
+			}
         }
     }
 }
